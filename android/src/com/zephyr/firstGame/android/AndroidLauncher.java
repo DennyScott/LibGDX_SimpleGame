d696a6d07a19a617854ae36ec4d0fa721b67a7ab
Denny Scott <dennyscott301@gmail.com>
2014-11-27 09:43:34 -0600

    add game

---
+++ android/src/com/zephyr/firstGame/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.zephyr.firstGame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.zephyr.firstGame.MyDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new MyDemo(), config);
+	}
+}
---
+++ desktop/src/com/zephyr/firstGame/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.zephyr.firstGame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zephyr.firstGame.MyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new MyDemo(), config);
+	}
+}
